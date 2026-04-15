021a0379e5e86cb7492d403061bf6481ebaba619
VitorEmmanuelP <104798951+VitorEmmanuelP@users.noreply.github.com>
2026-04-15 00:18:17 +0000

    chore: atividade02

---
+++ 7-periodo-main/atividade-01/src/config/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCiL8t-lZ8nJfnv1ho7iZH4tLELsnUxRZ4",
+  authDomain: "pratica-02-e08c8.firebaseapp.com",
+  projectId: "pratica-02-e08c8",
+  storageBucket: "pratica-02-e08c8.firebasestorage.app",
+  messagingSenderId: "352924224397",
+  appId: "1:352924224397:web:175fa7792edbfa00044bc4"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
